69f1e129ff7fe5705796e3e9ee907de149c6692c
Amrita-coder <amritachaturvedi094@gmail.com>
2025-05-11 18:59:09 +0530

    conditional statement

---
+++ if_else_statement.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+            ///// ////////basic /////////////////
+
+int main(){                         
+    int age =13;
+    if(age>=18){
+        printf("you are an adult");
+    }
+    else{
+        printf("you are minor");
+    }
+///////////////check odd even ///////////////////
+int a;
+printf("enter no: %d ");
+scanf("%d",&a);
+if(a%2 == 0){
+    printf("even no");
+}
+else{
+    printf("odd no");
+}
+
+return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- typeconversion.c
+++ typeconversion.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-int mainI(){
+int main(){
     int a =  (int) 1.99999;
     printf("%d \n",a);
     return 0;
