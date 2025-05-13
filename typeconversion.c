50c6422df396a1d096a9cad03156ef109388d4a2
Amrita-coder <amritachaturvedi094@gmail.com>
2025-05-13 21:27:01 +0530

    print slice

--- typeconversion.c
+++ typeconversion.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
+#include<string.h>
+void slice( char str[],int n , int m);
 
 int main(){
-    int a =  (int) 1.99999;
-    printf("%d \n",a);
+    
     return 0;
+}
+void slice( char str[],int n , int m){
+    char newsString[100];
+    int j = 0;
+    for(int i=n;i<=m;i++,j++){
+        newS
+    } 
 }
